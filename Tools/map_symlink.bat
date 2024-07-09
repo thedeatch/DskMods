761c10c1913cb7c4269b0a06d1379f4f148fdcdb
TriForceX <triforce@gznetwork.com>
2024-07-09 01:45:56 -0400

    [Tools] Arreglo creacion carpeta maps y obligar a abrir como admin

--- Tools/map_symlink.bat
+++ Tools/map_symlink.bat
@@ -1,9 +1,22 @@
 @echo off
+:: Check for administrative privileges
+net session >nul 2>&1
+if %errorlevel% neq 0 (
+    echo This script is NOT running with administrative privileges.
+    echo Please run the script as an administrator.
+    pause
+    exit /b
+)
 
 :: Set options
 set /p map_name="Map Name (No Extension): "
 set /p source_dir="Map Folder (Source): "
 set /p target_dir="Base Folder (Target): "
+
+:: Check if the maps folder exists
+if not exist "%target_dir%\maps" (
+    mkdir "%target_dir%\maps"
+)
 
 :: Copy main map files
 mklink "%target_dir%\maps\%map_name%.map" "%source_dir%\maps\%map_name%.map"
